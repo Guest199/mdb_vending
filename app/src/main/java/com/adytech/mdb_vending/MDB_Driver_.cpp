73936d0d158915be2cb760731a8815d1baee09ea
Guest199 <yaniv33@yahoo.com>
2025-01-01 15:10:26 +0200

    fix

--- app/src/main/java/com/adytech/mdb_vending/MDB_Driver_.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by ronen on 12/5/2024.
-//
-
-#include "MDB_Driver_.h"
--- app/src/main/java/com/adytech/mdb_vending/MDB_Driver_.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by ronen on 12/5/2024.
-//
-
-#ifndef MDB_VENDING_MDB_DRIVER__H
-#define MDB_VENDING_MDB_DRIVER__H
-
-#endif //MDB_VENDING_MDB_DRIVER__H
